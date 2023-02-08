9549f0ed2a71a9ce07851de2527467f478e22192
MLACIsGIT <molnar.laszlo@selester.hu>
2023-02-08 11:39:25 +0100

    controllers.heartbeat: db.initiateConnection and db.dropConnection added

--- backend/src/controllers/heartbeat.js
+++ backend/src/controllers/heartbeat.js
@@ -1,7 +1,9 @@
 import { PING as sqlPing } from '../db/storedProcedures';
 import { ftp } from '../ftp/ftpConnection';
+import { db, sqlConfig } from '../db/dbConnection';
 
 export const heartbeat = async (req, res) => {
+  let connected = false;
   const result = {
     heartbeat: true,
     version: '2022.09.26-01',
@@ -10,12 +12,20 @@ export const heartbeat = async (req, res) => {
     countOfStatusesInQuene: 'n/a',
   }
   try {
+    db.initiateConnection(sqlConfig);
+    connected = true;
+
     const sqlResult = await sqlPing();
-  
+
     result.dbConnection = true;
     result.countOfStatusesInQuene = sqlResult.countOfStatusesInQuene;
   } catch (error) {
     //Nothing to do
+  }
+
+  if (connected) {
+    db.dropConnection();
+    connected = false;
   }
 
   result.ftpConnection = await ftp.ping();
--- cron/exportStatuses.js
+++ cron/exportStatuses.js
@@ -1,11 +1,24 @@
 import fetch from 'node-fetch';
 
-console.log('process.env.BACKEND_URL', process.env.BACKEND_URL)
+const currentDate = new Date();
 
+console.clear();
+console.log('process.env.BACKEND_URL', process.env.BACKEND_URL)
 console.log('exportStatuses.js ...')
-await fetch(`${process.env.BACKEND_URL}/datachange/statuses`,
-{
-    method: 'PUT',
-    body: {}
-})
-console.log('... ended')
+console.log(`current time: ${currentDate}`)
+//nem kell await a fetch elé
+
+try {
+    fetch(`${process.env.BACKEND_URL}/datachange/statuses`,
+    {
+        method: 'PUT',
+        body: {}
+    }).then(() => {
+        console.log('... ended')
+     }).catch((error) => {
+        console.log('... ended with error')
+        console.error(error);
+    })        
+} catch (error) {
+    console.error('cron error', error)
+}
